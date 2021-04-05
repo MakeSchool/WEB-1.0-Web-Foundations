319af7e5726bbcf8b7e22216c8cdf6216091b4ec
rob cobb <rwcobbjr@gmail.com>
2021-04-05 19:44:22 -0400

    minor fixes for content

--- T002-HTML-syntax/HtmlDemo.js
+++ T002-HTML-syntax/HtmlDemo.js
@@ -3,7 +3,7 @@ import {CodeHighlighter} from 'components/atoms';
 
 const styles = {
   wrapper: {display: 'flex', flexFlow: 'row wrap'},
-  tab: {display: 'flex', flexFlow: 'column nowrap', margin: 16},
+  tab: {display: 'flex', flex: '1', flexFlow: 'column nowrap', margin: 16, maxWidth: '450px'},
   label: {
     border: '1px solid black',
     borderTopRadius: 2,
