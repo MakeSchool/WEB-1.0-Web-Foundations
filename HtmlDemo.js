f7a2ca5adba2c21ca444d7649a3041868f0e08ee
rob cobb <rwcobbjr@gmail.com>
2021-05-05 16:40:03 -0400

    shape up code demo styles

--- HtmlDemo.js
+++ HtmlDemo.js
@@ -1,31 +1,80 @@
 import React from 'react';
 import {CodeHighlighter} from 'components/atoms';
+import * as demoStyles from './demo.module.css';
 
 const styles = {
-  wrapper: {display: 'flex', flexFlow: 'row wrap'},
-  tab: {display: 'flex', flex: '1', flexFlow: 'column nowrap', margin: 16, maxWidth: '450px'},
+  wrapper: {
+    display: 'flex',
+    flexFlow: 'row wrap',
+    margin: '16px 0',
+    padding: '8px 0',
+    border: '1px solid #2D3748',
+    backgroundColor: '#2D3748',
+    borderRadius: '2px',
+  },
+  tab: {
+    display: 'flex',
+    flex: '1',
+    flexFlow: 'column nowrap',
+    margin: '2px',
+    maxWidth: '380px',
+    minWidth: '380px',
+  },
   label: {
-    border: '1px solid black',
-    borderTopRadius: 2,
-    borderBottom: 'none',
-    padding: 4,
-    margin: 4,
+    width: 'max-content',
+    padding: 6,
+    margin: '0 4px 4px 4px',
+    textTransform: 'uppercase',
+    color: 'rgb(238, 238, 239)',
+    fontWeight: 'bold',
+  },
+  code: {
+    height: 230,
   },
   output: {
-    height: '100%',
+    height: 230,
+    border: 'none',
+    backgroundColor: 'rgb(255,255,255)',
+    borderRadius: '0.3em',
   },
 };
 
-const HtmlDemo = ({srcString, highlightLines}) => {
+const codeBlocks = nodes =>
+  nodes
+    .filter(node => node.props.originalType === 'pre')
+    .map(pre => pre.props.children)
+    .filter(codeEl => codeEl.props.mdxType === 'code');
+
+const makeSrcString = nodes => {
+  return codeBlocks(nodes)
+    .map(block => {
+      if (block.props.className === 'language-js') {
+        return `<script>\n${block.props.children}\n</script>`;
+      } else if (block.props.className === 'language-css') {
+        return `<style>\n${block.props.children}\n</style>`;
+      } else if (block.props.className === 'language-html') {
+        return block.props.children;
+      } else {
+        throw new Error('JsDemo code blocks must be html, css, or js');
+      }
+    })
+    .join('\n\n');
+};
+
+const HtmlDemo = ({children, srcString}) => {
+  const src = srcString || makeSrcString(React.Children.toArray(children));
+  const display = children || <CodeHighlighter>{src}</CodeHighlighter>;
   return (
     <div style={styles.wrapper}>
       <div style={styles.tab}>
         <div style={styles.label}>Code</div>
-        <CodeHighlighter>{srcString}</CodeHighlighter>
+        <div className={demoStyles.code} style={styles.code}>
+          {display}
+        </div>
       </div>
       <div style={styles.tab}>
         <div style={styles.label}>Result</div>
-        <iframe style={styles.output} srcDoc={srcString}></iframe>
+        <iframe style={styles.output} srcDoc={src}></iframe>
       </div>
     </div>
   );
--- MagicDemo.js
+++ MagicDemo.js
@@ -1,18 +1,6 @@
 import React, {useState} from 'react';
 import HtmlDemo from './HtmlDemo';
 
-const styles = {
-  wrapper: {display: 'flex', flexFlow: 'row wrap'},
-  tab: {display: 'flex', flex: '1', flexFlow: 'column nowrap', margin: 16, maxWidth: '450px'},
-  label: {
-    border: '1px solid black',
-    borderTopRadius: 2,
-    borderBottom: 'none',
-    padding: 4,
-    margin: 4,
-  },
-};
-
 const MagicDemo = ({input, template, initialState = {}}) => {
   const [inputState, setInputState] = useState(initialState);
 
