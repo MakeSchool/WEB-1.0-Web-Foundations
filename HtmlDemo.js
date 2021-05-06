4a519a7d615013838e15cde468185790cf9f7a1c
rob cobb <rwcobbjr@gmail.com>
2021-05-06 16:01:34 -0400

    pixel-pushing magic demos

--- HtmlDemo.js
+++ HtmlDemo.js
@@ -3,29 +3,12 @@ import {CodeHighlighter} from 'components/atoms';
 import * as demoStyles from './demo.module.css';
 
 const styles = {
-  wrapper: {
-    display: 'flex',
-    flexFlow: 'row wrap',
-    margin: '16px 0',
-    padding: '4px 4px 8px',
-    border: '1px solid #2D3748',
-    backgroundColor: '#2D3748',
-    borderRadius: '4px',
-  },
   tab: {
     display: 'flex',
     flex: '1',
     flexFlow: 'column nowrap',
     margin: '2px',
     minWidth: '380px',
-  },
-  label: {
-    width: 'max-content',
-    padding: 6,
-    margin: '0 4px 4px 4px',
-    textTransform: 'uppercase',
-    color: 'rgb(238, 238, 239)',
-    fontWeight: 'bold',
   },
   code: {
     height: 230,
@@ -64,15 +47,15 @@ const HtmlDemo = ({children, srcString}) => {
   const src = srcString || makeSrcString(React.Children.toArray(children));
   const display = children || <CodeHighlighter>{src}</CodeHighlighter>;
   return (
-    <div style={styles.wrapper}>
+    <div className={demoStyles.wrapper}>
       <div style={styles.tab}>
-        <div style={styles.label}>Code</div>
+        <div className={demoStyles.label}>Code</div>
         <div className={demoStyles.code} style={styles.code}>
           {display}
         </div>
       </div>
       <div style={styles.tab}>
-        <div style={styles.label}>Result</div>
+        <div className={demoStyles.label}>Result</div>
         <iframe style={styles.output} srcDoc={src}></iframe>
       </div>
     </div>
--- MagicDemo.js
+++ MagicDemo.js
@@ -1,15 +1,34 @@
 import React, {useState} from 'react';
 import HtmlDemo from './HtmlDemo';
+import * as styles from './demo.module.css';
+
+const MagicInput = ({name, type = 'text', state, updateState}) => (
+  <div>
+    <label for={name}>{name}</label>
+    <input
+      type={type}
+      name={name}
+      id={name}
+      value={state[name]}
+      onChange={e => updateState({...state, [name]: e.target.value})}
+    />
+  </div>
+);
 
 const MagicDemo = ({input, template, initialState = {}}) => {
   const [inputState, setInputState] = useState(initialState);
 
   return (
-    <>
-      {input(inputState, setInputState)}
+    <div className={styles.magic}>
+      <div className={styles.label}>Playground</div>
+      <div className={styles.hint}>
+        Try changing these values. See what changes in the code and in the output.
+      </div>
+      <div className={styles.inputs}>{input(inputState, setInputState)}</div>
       <HtmlDemo srcString={template(inputState)} />
-    </>
+    </div>
   );
 };
 
 export default MagicDemo;
+export {MagicInput};
